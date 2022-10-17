adf05286fb5141b10697a993ad08d4a6bd5b30d6
matthewtimrobert <matthewtimroberts@gmail.com>
2022-10-17 16:42:05 -0400

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.scss";
 import { ConfigureStore } from "./redux/configureStore";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -19,8 +18,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -5,5 +5,5 @@ import reducer from "./reducer";
 export const ConfigureStore = () => configureStore({ reducer });
 
 export type RootState = ReturnType<typeof reducer>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
