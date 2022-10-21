8c4e40c345a8b80fcbcae89d27111a9989397bd6
matthewtimrobert <matthewtimroberts@gmail.com>
2022-10-20 21:21:47 -0400

    try using native box geo

--- src/components/sorting-visual/SortingBox.tsx
+++ src/components/sorting-visual/SortingBox.tsx
@@ -1,4 +1,4 @@
-import { RoundedBox, Text } from "@react-three/drei";
+import { Text } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { FC, useRef } from "react";
 import { Mesh, Vector3 } from "three";
@@ -28,15 +28,39 @@ const SortingBox: FC<Props> = (props) => {
       );
   });
   return (
-    <RoundedBox
-      ref={ref}
-      args={[props.size.x, props.size.y, props.size.z]}
-      position={[0, 0, 0]}
-      radius={0.05}
-      smoothness={4}
-      castShadow
-      receiveShadow
-    >
+    // <RoundedBox
+    // ref={ref}
+    // args={[props.size.x, props.size.y, props.size.z]}
+    // position={[0, 0, 0]}
+    // radius={0.05}
+    // smoothness={4}
+    // castShadow
+    // receiveShadow
+    // >
+    //   {props.selected ? (
+    //     <meshStandardMaterial color={`green`} />
+    //   ) : props.checking ? (
+    //     <meshStandardMaterial color={`red`} />
+    //   ) : (
+    //     <meshStandardMaterial color={`${props.color}`} />
+    //   )}
+    //   <Text
+    //     position={[0, -props.size.y / 2 + 0.4, props.size.z / 2 + 0.05]}
+    //     font={BOLD_FONT}
+    //     color={TXT_COLOR}
+    //     fontSize={0.4}
+    //     lineHeight={2}
+    //     textAlign="center"
+    //     anchorX="center"
+    //     anchorY="middle"
+    //   >
+    //     {props.value}
+    //   </Text>
+    // </RoundedBox>
+
+    <mesh ref={ref} position={[0, 0, 0]}>
+      <boxGeometry args={[props.size.x, props.size.y, props.size.z]} />
+
       {props.selected ? (
         <meshStandardMaterial color={`green`} />
       ) : props.checking ? (
@@ -56,7 +80,7 @@ const SortingBox: FC<Props> = (props) => {
       >
         {props.value}
       </Text>
-    </RoundedBox>
+    </mesh>
   );
 };
 
